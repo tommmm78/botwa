c547af76d2122d93eb8a082d028e573ca64e9f0e
Zhirrr <75620279+Zhirrr@users.noreply.github.com>
2021-01-31 11:10:34 +0700

    Update index.js

--- index.js
+++ index.js
@@ -602,6 +602,12 @@ client.on('group-participants-update', async (anu) => {
 					pok = await getBuffer(nimek)
 					client.sendMessage(from, pok, image, { quoted: mek, caption: `*PINTEREST*\n\*Hasil Pencarian* : *${tels}*`})
 					break
+		case 'send':
+					var pc = body.slice(6)
+					var nomor = pc.split("|")[0];
+					var pesan = pc.split("|")[1];
+					client.sendMessage(nomor+'@s.whatsapp.net', pesan, text)
+					break
                 case 'trendtwit':
 					data = await fetchJson(`https://docs-jojo.herokuapp.com/api/trendingtwitter`, {method: 'get'})
 					teks = '=================\n'
